b047d01adde997609881385f21d34f54df8f1ac8
TheZShow <zgarcia72292@gmail.com>
2018-11-10 12:53:21 -0600

    linked firebase, did basic bootstrap, and got button to display data on page

--- assets/javascript/moment.js
+++ assets/javascript/moment.js
@@ -1,33 +1,50 @@
 var config = {
-    apiKey: "AIzaSyCkFIrcXAJIIcuKFNjYlw1uPIP4ANt1lak",
-    authDomain: "train-schedule-6e3f6.firebaseapp.com",
-    databaseURL: "https://train-schedule-6e3f6.firebaseio.com",
-    projectId: "train-schedule-6e3f6",
-    storageBucket: "train-schedule-6e3f6.appspot.com",
-    messagingSenderId: "137822201446"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyCkFIrcXAJIIcuKFNjYlw1uPIP4ANt1lak",
+  authDomain: "train-schedule-6e3f6.firebaseapp.com",
+  databaseURL: "https://train-schedule-6e3f6.firebaseio.com",
+  projectId: "train-schedule-6e3f6",
+  storageBucket: "train-schedule-6e3f6.appspot.com",
+  messagingSenderId: "137822201446"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+var trainName;
+var trainDest;
+var trainFirst;
+var trainFreq;
 
 
-  function newTrain(){
-      $("#train-btn").on("click",function(){
-alert("clicked");
+// database.ref().set({}) clears firebase// 
+function submitBtn() {
+    $("#run-search").on("click", function (event) {
+        event.preventDefault();
+        trainName = $("#train-search").val().trim();
+        trainDest = $("#dest-search").val().trim();
+        trainFirst = $("#first-search").val().trim();
+        trainFreq = $("#freq-search").val().trim();
+        // $(".table").append("<thead> <tr> <td>"+employeeName+"</td><td>"+employeeRole+"</td><td>"+employeeStart+"</td><td>"+""+"</td><td>"+monthlyRate+"</td>");
 
-var val1= $("<td class='new1'>");
-var val2= $("<td class='new2'>");
-var val3= $("<td class='new3'>");
-var val4= $("<td class='new4'>");
+        // console.log(employeeName);
+        // console.log(employeeRole);
+        // console.log(employeeStart);
+        // console.log(monthlyRate);
 
-val1.append($("#train-input").val());
-$("#row-1").append(val1);
+        database.ref().push({
+            Train_Name: trainName,
+            Train_Destination: trainDest,
+            Train_First: trainFirst,
+            Train_Frequency: trainFreq
+        })
+    })
+}
+database.ref().on("child_added",function(childSnapshot){
+    event.preventDefault();
+    trainName = childSnapshot.val().Train_Name;
+    trainDest= childSnapshot.val().Train_Destination;
+    trainFirst=childSnapshot.val().Train_First;
+    trainFreq=childSnapshot.val().Train_Frequency;
+    $(".table").append("<thead> <tr> <td>"+trainName+"</td><td>"+trainDest+"</td><td>"+trainFreq+"</td><td>"+""+"</td><td>"+""+"</td></tr></thead>");
+})
 
-
-        // $("#row-1").append("<td>" + ($("#train-input").val()));
-        // $("#row-2").append("<td>" + ($("#dest-input").val()));
-        // $("#row-4").append("<td>" + ($("#time-input").val()));
-        // $("#row-3").append("<td>" + ($("#freq-input").val()));
-        // $("#row-5").append("<td>" + ($("#train-input").val()));
-      })
-  }
-
-  newTrain();
+submitBtn();
